94f5fc4677ae81cd1dadd5c99ae3ae270f58700b
Valerian <@VelValerian>
2023-11-06 00:00:01 +0200

    v.0.4.2 Refactor name of handlers Add handler "Stop", "Statistic" Add to handler functions from db

--- app/handlers/handl.py
+++ app/handlers/handl.py
@@ -10,7 +10,7 @@ from app.keyboards import kb_reply
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    # await db_commands.db_start()
+    await db_commands.db_add_new_user(int(message.from_user.id))
     await message.answer(f"Hi {message.from_user.username} "
                          "\n\nI'm a simple Bot that can store trivias in library."
                          "\nYou can store books or simple things that interest you.",
@@ -31,57 +31,64 @@ async def cmd_begin(message: Message):
     await message.delete()
 
 
+@dp.message(F.text == "Stop")
+async def cmd_stop(message: Message):
+    await message.answer("Let's Start", reply_markup=kb_reply.begin_keyboard)
+    await db_commands.db_stop_activity(message.from_user.id)
+    await message.delete()
+
+
+@dp.message(F.text == "Statistics")
+async def cmd_statistics(message: Message):
+
+    await message.answer(f"Here Statistic", reply_markup=kb_reply.begin_keyboard)
+    await message.delete()
+
+
 @dp.message(F.text == "Choose activity")
-async def cmd_help(message: Message):
+async def cmd_choose(message: Message):
     await message.answer("Pick a topic of activity",
                          reply_markup=kb_reply.activity_keyboard)
     await message.delete()
 
 
-@dp.message(F.text == "Stop")
-async def cmd_help(message: Message):
-    await message.answer("You press STOP buttons",
-                         reply_markup=kb_reply.begin_keyboard)
-    await message.delete()
-
-
 @dp.message(F.text == "Rest")
-async def cmd_help(message: Message):
+async def cmd_rest(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.rest_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Health")
-async def cmd_help(message: Message):
+async def cmd_health(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.health_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Routine")
-async def cmd_help(message: Message):
+async def cmd_routine(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.routine_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Study")
-async def cmd_help(message: Message):
+async def cmd_study(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.study_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Job")
-async def cmd_help(message: Message):
+async def cmd_job(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.job_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Road to")
-async def cmd_help(message: Message):
+async def cmd_road(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.road_keyboard)
     await message.delete()
@@ -95,27 +102,30 @@ async def cmd_help(message: Message):
                         "Reading", "Videos", "Learning", "Coding", "At work",
                         "At home", "To work", "To home", "To study", "To meeting",
                         "To party / hobby / fun"}))
-async def cmd_begin(message: Message):
+async def cmd_start_activity(message: Message):
     await message.answer("Press Stop when complete activity", reply_markup=kb_reply.stop_keyboard)
+    await db_commands.db_start_activity(message.text, message.from_user.id)
     await message.delete()
 
 
 @dp.message(F.text == "Sports exercises")
-async def cmd_help(message: Message):
+async def cmd_sport(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.health_sports_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Health routine")
-async def cmd_help(message: Message):
+async def cmd_health_routine(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.health_routine_keyboard)
     await message.delete()
 
 
 @dp.message(F.text == "Health care")
-async def cmd_help(message: Message):
+async def cmd_health_care(message: Message):
     await message.answer("Pick activity",
                          reply_markup=kb_reply.health_care_keyboard)
     await message.delete()
+
+
